7f0f86d7be3c3a8f802f8ba64f7799bdaf461d1f
Alycia Cahill <acahil@uw.edu>
2019-05-12 08:38:29 -0700

    repo set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+  _handResponseJson(json) {
+    this.setState({
+
+    });
+  }
+  componentDidMount() {
+    fetch('https://rickandmortyapi.com/api/')
+    .then( res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error.message));
+  }
 }
 
 export default App;
